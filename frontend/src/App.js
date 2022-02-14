62b112ec595c693f3b59316090650184d6d71e13
Vakantanka <laszlo.fenyvesi@gmail.com>
2022-02-14 20:29:53 +0100

    paginator bent van csak tesztelni, de még nem megy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { getData} from './api';
+import { getData } from './api';
+import Paginator from './components/Paginator.jsx';
 
 function App() {
   const [records, setRecords] = useState(null);
-  // const [images, setImages] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  let data={};
+  let max=0;
+
+  const decreasePageNum = () => {
+    pageNum > 1 ? setPageNum(pageNum-1) : setPageNum(pageNum);
+  }
+
+  const increasePageNum = () => {
+    pageNum < max ? setPageNum(pageNum+1) : setPageNum(pageNum);
+  }
 
   useEffect(() => {
     const init = async() => {
-      const data = await getData();
-      // const imageData = await getImageData();
+      data = await getData(1);
       console.log(data);
-      // console.log(imageData);
       setRecords(data.records);
-      // setImages(imageData.records);
+      max=data.info.pages;
     };
     init();
-  }, []);
+  }, [pageNum]);
 
   return (
     <div>
-      <h1>Hello</h1>
-      {/* <div className='images'>
-        {images ?
-        images.map(image => (
-        <div key={image.id}>
-          <p>{image.id}</p>
-          <img alt={image.id} src={image.baseimageurl}/>
-        </div>
-        )) : 
-        <p>Loading...</p>}
-      </div> */}
-      <div className='records'>
+      <h1>Artwork</h1>
+      <div className='artRecords'>
         {records ?
-        records.map(record => (
-        <div key={record.id}>
-          <p>{record.id}</p>
-          {record.imagecount ? 
-            // <img alt={record.id} src={`https://ids.lib.harvard.edu/ids/iiif/${record.id}/full/full/0/default.jpg`}/> {/* A record.id nem jó a source-ba! */}
-            <img alt={record.id} src={record.primaryimageurl} /> : ""}
-          <p>{record.title}</p>
-          <p>{record.classification}</p>
-          <p>{record.dated}</p>
-        </div>
-        )) : 
-        <p>Loading...</p>}
+          records.map(record => (
+          <div key={record.id} className="artItem" >
+            {record.primaryimageurl ? 
+              <img alt={record.id} src={record.primaryimageurl} /> : <p>No image</p>}
+            <h3>{record.title}</h3>
+            <p className="classification"><span className='label'>Classification</span>{record.classification}</p>
+            <p className="workType"><span className='label'>Work type</span>{record.worktypes.map((item, index) => item.worktype)}</p>
+            <p className="century">{record.century}</p>
+          </div>
+          )) : 
+          <p>Loading...</p>
+        }
       </div>
+      {records ?
+        <Paginator pageNum={pageNum} onForward={increasePageNum} onBackward={decreasePageNum} max={max} /> : ""
+      }
     </div>
   );
 }
