49059966d2113b1840e876a41efe46dbf9af8ad7
Alexander Nagy <nagyal3x@gmail.com>
2022-02-16 02:02:11 +0100

    Sign in :wrench:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getData } from "./api";
 import Card from "./components/Card";
+import SignInSide from "./components/SignInSide";
 
 function App() {
   const [records, setRecords] = useState(null);
+  const [loginClicked, setLoginClicked] = useState(false);
 
   useEffect(() => {
     const init = async () => {
@@ -15,11 +17,16 @@ function App() {
     init();
   }, []);
 
+  const loginClickHandler = () => { 
+    setLoginClicked(!loginClicked)
+   }
+
   return (
     <div>
       <nav>
-        <button>Login</button>
+        <button onClick={loginClickHandler} className="login-btn">Log in</button>
       </nav>
+      {loginClicked && <SignInSide />} 
       <h1>Artwork</h1>
       <div className="artRecords">
         {records ? (
