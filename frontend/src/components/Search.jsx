47132fc15d258b725a4a22261e5577991bedfb13
Vakantanka <laszlo.fenyvesi@gmail.com>
2022-02-22 14:16:25 +0100

    clear search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { MdImageSearch } from "react-icons/md";
+import { FiDelete } from "react-icons/fi";
 
 const Search = ({searchString, setSearchString, search}) => {
   return (
     <div>
-      <form onSubmit={(e) => {
+      <form className="searchForm" onSubmit={(e) => {
           e.preventDefault();
           search()
         }}>
         <input type="text" name="search" className="searchInput" placeholder="search" value={searchString} onChange={(e) => setSearchString(e.target.value)}/>
+        { searchString.length > 0 ? <button className="clearButton" onClick={() => setSearchString("")} title="clear search"><FiDelete /></button> : ""}
         <button className="searchButton"><MdImageSearch /></button>
         </form>
     </div>
